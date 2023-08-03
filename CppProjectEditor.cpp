15c2673779c5bdfe4fb0ab289a32aee6c25d263d
Robert Powell <121625312+Erasioo@users.noreply.github.com>
2023-08-03 09:41:33 -0400

    Update CppProjectEditor.cpp

--- CppProjectEditor.cpp
+++ CppProjectEditor.cpp
@@ -1,17 +1,5 @@
-#pragma once
-
-#include "Engine.h"
-#include "Modules/ModuleInterface.h"
+#include "NewCppTutorialEditor.h"
 #include "Modules/ModuleManager.h"
-#include "UnrealEd.h"
+#include "Modules/ModuleInterface.h"
 
-#include "MoveComponentVisualizer.h"
-
-DECLARE_LOG_CATEGORY_EXTERN(NewCppTutorialEditor, All, All)
-
-class FNewCppTutorialEditorModule: public IModuleInterface
-{
-public:
-    void StartupModule() override;
-    void ShutdownModule() override;
-};
+IMPLEMENT_GAME_MODULE(FNewCppTutorialEditorModule, NewCppTutorialEditor);
